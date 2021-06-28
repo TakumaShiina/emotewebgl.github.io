e1acecffaeabd293e45e55c4a3f14cc0483e8dbc
M.Shiina <shiina@mtwo.co.jp>
2021-06-28 13:42:17 +0900

    shisuhaの表示テスト

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function start() {
     // create hidden canvas for rendering
-    EmotePlayer.createRenderCanvas(2048, 2048);
-    EmotePlayer.maskMode = EmotePlayer.MaskMode.ALPHA;
-
+    EmotePlayer.createRenderCanvas(1024, 1024);
 
 /*
     // EmoteDevice is a class that manages shaders and GPU resources necessary for drawing Emote.
@@ -15,26 +13,14 @@ function start() {
     EmotePlayer.requireDevice();
 */
 
-    // create new player nomal_size
-    const canvas_nomal = document.getElementById('canvas_nomal');
-    const player_nomal = new EmotePlayer(canvas_nomal);
+    // create new player
+    const canvas = document.getElementById('canvas');
+    const player = new EmotePlayer(canvas);
     // load emote data from URL
-    player_nomal.loadDataFromURL("data/emote_asobirin_normal.emtbytes");
+    player.loadDataFromURL("data/shisuha_01.emtbytes");
     // set chara scale
-    player_nomal.scale = 1;
+    player.scale = 1;
     // play test timeline
-    player_nomal.mainTimelineLabel = 'sample_全自動_test';
-
-    // create new player small_size
-    const canvas_small = document.getElementById('canvas_small');
-    const player_small = new EmotePlayer(canvas_small);
-    // load emote data from URL
-    player_small.loadDataFromURL("data/emote_woman_quarter_210118.emtbytes");
-    // set chara scale
-    player_small.scale = 0.6;
-    // play test timeline
-    player_small.mainTimelineLabel = 'sample_全自動_test';
-
-
+    player.mainTimelineLabel = 'sample_全自動_test';
 }
 
